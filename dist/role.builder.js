89fac1fe6b0c0ceccf63fcd2e582914161918ab6
Daniel Luederwald <das_flip@gmx.de>
2016-06-27 20:35:09 +0200

    Let creeps try to get energy from containers first before defaulting to sources

--- dist/role.builder.js
+++ dist/role.builder.js
@@ -25,9 +25,20 @@ var roleBuilder = {
       }
     }
     else {
+      var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (structure.structureType == STRUCTURE_CONTAINER) && (_.sum(structure.store) > creep.carryCapacity);
+          }
+        });
+      if(container) {
+        if(container.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(container, {reusePath: 5});
+        }
+      } else {
       var source = creep.pos.findClosestByRange(FIND_SOURCES);
       if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
         creep.moveTo(source, {reusePath: 5});
+      }
       }
     }
   }
--- dist/role.upgrader.js
+++ dist/role.upgrader.js
@@ -11,10 +11,21 @@ var roleUpgrader = {
       creep.memory.upgrading = true;
     }
     if(!creep.memory.upgrading) {
+      var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (structure.structureType == STRUCTURE_CONTAINER) && (_.sum(structure.store) > creep.carryCapacity);
+          }
+        });
+      if(container) {
+        if(container.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(container, {reusePath: 5});
+        }
+      } else {
       var source = creep.pos.findClosestByRange(FIND_SOURCES);
       if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
         creep.moveTo(source, {reusePath: 5});
       }
+}
     }
     else {
       if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
